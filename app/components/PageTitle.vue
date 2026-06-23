e0734386a5588a1a850caa60b347e8a81d92e9cf
Craig <cg37@outlook.com>
2026-06-23 19:49:53 +0800

    feat: add responsive font styles and theme variables in SCSS files

---
+++ app/components/PageTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page_title">
+        {{ title }}
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    title: string
+}>()
+</script>
+
+<style lang="scss" scoped>
+.page_title {
+    font-size: 32px;
+    font-width: 800;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,18 +10,14 @@ export default defineNuxtConfig({
     srcDir: 'app/',
     compatibilityDate: '2026-06-20',
     devtools: { enabled: true },
-    css: [
-        '@/assets/css/print.scss',
-        '@/assets/css/main.scss',
-        '@/assets/css/fonts.scss',
-        '@/assets/css/colorMode.scss',
-    ],
+    css: ['@/assets/css/print.scss', '@/assets/css/main.scss', '@/assets/css/fonts.scss'],
 
     modules: ['@nuxtjs/color-mode'],
     colorMode: {
         preference: 'light',
         fallback: 'light',
         classSuffix: '',
+        storage: 'cookie',
     },
 
     debug: process.env.NODE_ENV !== 'production',
